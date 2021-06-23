2c90d85caf8d898eae82e0a930cefce3c616343c
Artem Kolinko <kalinart@gmail.com>
2021-06-23 19:50:56 +0300

    Add tasks 3-4

--- src/problems/03-task.js
+++ src/problems/03-task.js
@@ -1,27 +1,61 @@
 const task3 = {
-    /*
+  /*
     isPalindrome('a') => true;
     isPalindrome('abc') => false;
     isPalindrome('aabbaa') => true;
      */
-    isPalindrome: function(str) {
+  isPalindrome: function (str) {
+    const reverse = str.split('').reverse().join('');
+    return str === reverse;
+  },
 
-    },
-
-    /*
+  /*
     charCount('abc') => a: 1, b: 1, c: 1
     charCount('Hello') => H: 1, e: 1, l: 2, o: 1
      */
-    charCount: function(str) {
+  charCount: function (str) {
+    const charObj = {};
 
-    },
+    const charArr = str.split('');
 
-    /*
+    charArr.forEach((char) => {
+      if (charObj[char]) return charObj[char]++;
+      charObj[char] = 1;
+    });
+
+    let result = '';
+
+    for (const key in charObj) {
+      if (Object.hasOwnProperty.call(charObj, key)) {
+        const value = charObj[key];
+        result += `${key}: ${value}, `;
+      }
+
+      // можно ли без hasOwnProperty?
+      // const value = charObj[key];
+      // result += `${key}: ${value}, `;
+    }
+
+    return result;
+  },
+
+  /*
     areAnagrams('anagram', 'nag a ram') => true;
     areAnagrams('Eleven plus two', 'Twelve plus one'); => true;
     areAnagrams('O, Draconian devil', 'Leonardo da Vinci') => true;
      */
-    areAnagrams: function(str1, str2) {
+  areAnagrams: function (str1, str2) {
+    const sortStr = (str) =>
+      str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
 
-    },
+    const sortedStr1 = sortStr(str1);
+    const sortedStr2 = sortStr(str2);
+
+    return sortedStr1 === sortedStr2;
+  }
 };
+
+// console.log(task3.isPalindrome('aabbaa'));
+// console.log(task3.charCount('Hello'));
+// console.log(task3.charCount('Hello'));
+// console.log(task3.areAnagrams('anagram', 'nag a ram'));
--- src/problems/04-task.js
+++ src/problems/04-task.js
@@ -1,25 +1,71 @@
 const task4 = {
-    httpGetRequest(url, params, callback) {
-        /**
-         * аргументы:
-         * url - адрес сервера
-         * params - объект, в котором хранятся параметры для запроса
-         * этот объект должен быть преобразован в строку и добавлен к url после символа '?'
-         * callback - ф-ция которая будет вызвана после того как запрос выполнен и данные удачно распарсились
-         * callback должен вызываться так:
-         * если случилась какая-то ошибка то callback(error), если ошибки не было то callback(null, data);
-         * использовать XmlHttpRequest 
-         * https://developer.mozilla.org/ru/docs/Web/API/XMLHttpRequest
-         *  
-         */
-    },
+  httpGetRequest(url, params, callback) {
+    /**
+     * аргументы:
+     * url - адрес сервера
+     * params - объект, в котором хранятся параметры для запроса
+     * этот объект должен быть преобразован в строку и добавлен к url после символа '?'
+     * callback - ф-ция которая будет вызвана после того как запрос выполнен и данные удачно распарсились
+     * callback должен вызываться так:
+     * если случилась какая-то ошибка то callback(error), если ошибки не было то callback(null, data);
+     * использовать XmlHttpRequest
+     * https://developer.mozilla.org/ru/docs/Web/API/XMLHttpRequest
+     *
+     */
+    const xhr = new XMLHttpRequest();
 
-    objectToQuery(obj) {
-        //ф-ция на вход получает объект {key1: value1, key2: value2,.... } и возваращет строку в виде key1=value1&key2=value2....
-    },
+    // Open request and use task4 method objectToQuery()
+    xhr.open('GET', url + '?' + this.objectToQuery(params));
+    xhr.send();
 
-    queryToObject(query) {
-        //ф-ция на вход получает строку в виде key1=value1&key2=value2.... и возваращет объект {key1: value1, key2: value2,.... }
+    xhr.onload = function () {
+      if (xhr.status != 200) {
+        const error = new Error(`Error ${xhr.status}: ${xhr.statusText}`);
+        callback(error);
+      } else {
+        const data = JSON.parse(xhr.response);
+        callback(null, data);
+      }
+    };
+
+    xhr.onerror = function () {
+      alert('Request failed');
+    };
+  },
+
+  objectToQuery(obj) {
+    //ф-ция на вход получает объект {key1: value1, key2: value2,.... } и возваращет строку в виде key1=value1&key2=value2....
+
+    let result = '';
+    for (const key in obj) {
+      if (Object.hasOwnProperty.call(obj, key)) {
+        const val = obj[key];
+        result += `${key}=${val}&`;
+      }
     }
 
+    // remove last &
+    result = result.slice(0, -1);
+
+    return result;
+  },
+
+  queryToObject(query) {
+    //ф-ция на вход получает строку в виде key1=value1&key2=value2.... и возваращет объект {key1: value1, key2: value2,.... }
+
+    const obj = {};
+
+    const propsArr = query.split('&');
+
+    propsArr.forEach((prop) => {
+      const [key, value] = prop.split('=');
+      obj[key] = value;
+    });
+
+    return obj;
+  }
 };
+
+// task4.httpGetRequest('https://jsonplaceholder.typicode.com/posts/1', { key1: 'value1', key2: 'value2' }, ()=>{})
+// console.log(task4.objectToQuery({ key1: 'value1', key2: 'value2' }));
+// console.log(task4.queryToObject('key1=value1&key2=value2'));
